40c924c5d214898eed7982de4ed6c218e5cdfccc
jubinayoob <jubin.ayoob@mountblue.io>
2018-06-19 09:28:38 +0530

    Adds test in client to check mocha setup

--- client/test/test.js
+++ client/test/test.js
@@ -2,6 +2,6 @@ const assert = require("chai").assert;
 
 describe("Tests for Client Section",function(){
     it("Checking Test Environment",function(){
-        
+        assert.isOk(true);
     });
 });
