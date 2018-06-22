13576f603a5ec38f620c1f7fc26466d8ee0a3da6
Abhishek Kumar <abhishek.kumar@mountblue.io>
2018-06-22 17:02:23 +0530

    update index route

--- client/src/react-app/index.js
+++ client/src/react-app/index.js
@@ -5,6 +5,8 @@ import Login from './container/Login';
 import Home from './container/Home';
 import Question from './container/QuestionAdd';
 import Editor from './container/Editor';
+import Business from './container/BusinessHome';
+import AddTest from './container/AddTest';
 
 // eslint-disable-next-line no-unused-vars
 class App extends React.Component {
@@ -15,7 +17,9 @@ class App extends React.Component {
           <Route path="/" exact component={Login} />
           <Route path="/home" component={Home} />
           <Route path="/question" component={Question} />
-          <Route path="/editor" component={Editor} />
+          <Route path="/editor" component={Editor}/>
+          <Route path="/business" component={Business} />
+          <Route path="/test" component={AddTest} />
         </div>
       </Router>
     );
