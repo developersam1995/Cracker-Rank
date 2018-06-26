6f8342aa939f9889cf59f029bba06d10f7e2023c
sam1995 <samrudh.kumar@mountblue.io>
2018-06-26 19:17:46 +0530

    Update form logiv

--- client/src/react-app/container/QuestionAdd.js
+++ client/src/react-app/container/QuestionAdd.js
@@ -13,7 +13,7 @@ class Question extends React.Component {
       exampleOutput: '',
       testCases: [],
       functionName: '',
-      paramNames: []
+      paramNames: ''
     };
 
     this.onChange = this.onChange.bind(this);
@@ -21,10 +21,7 @@ class Question extends React.Component {
   }
 
   onChange(event) {
-    this.setState({[event.target.name]: event.target.value},
-      () => {
-        console.log(this.state);
-      });
+    this.setState({[event.target.name]: event.target.value});
   }
 
   validateAndSubmit(event) {
@@ -34,8 +31,14 @@ class Question extends React.Component {
 
     const question = {
       title: this.state.title,
-      problemDescription: this.state.problemDescription
+      problemDescription: this.state.problemDescription,
+      exampleInput: this.state.exampleInput,
+      exampleOutput: this.state.exampleOutput,
+      functionName: this.state.functionName,
+      paramNames: this.state.paramNames.split(',')
     };
+
+    console.log(question);
 
     // fetch('url-to-post',{
     //   method: 'POST',
@@ -58,88 +61,47 @@ class Question extends React.Component {
           <div>
             <label>Title: </label> <br />
             <input type="text" name="title" value={this.state.title}
-              onChange={this.onChange} />
+              onChange={this.onChange} required/>
           </div>
           <br/>
 
           <div>
             <label>Problem Description: </label> <br />
             <textarea name="problemDescription" value={this.state.problemDescription}
-              onChange={this.onChange} />
+              onChange={this.onChange} required/>
           </div>
           <br />
 
           <div>
             <label>Example Inputs: </label> <br />
             <input type="text" name="exampleInput" value={this.state.exampleInput}
-              onChange={this.onChange} />
+              onChange={this.onChange} required/>
           </div>
           <br/>
 
           <div>
             <label>Example output: </label> <br />
             <input type="text" name="exampleOutput" value={this.state.exampleOutput}
-              onChange={this.onChange} />
+              onChange={this.onChange} required/>
           </div>
           <br/>
 
           <div>
             <label>Function name: </label> <br />
             <input type="text" name="functionName" value={this.state.functionName}
-              onChange={this.onChange} />
+              onChange={this.onChange} required/>
           </div>
           <br/>
 
-          
+           <div>
+            <label>Function parameter names(comma separated): </label> <br />
+            <input type="text" name="paramNames" value={this.state.paramNames}
+              onChange={this.onChange} required/>
+          </div>
+          <br/>
 
           <button type="submit">Submit</button>
         </form>
-        {/* <form method="POST" action="http://localhost:4001/api/v1/question" className="QuestionAddForm">
-          <label>Title</label>
-          <textarea name="title" rows="1" className="form-data"></textarea>
-          <label>Problem</label>
-          <textarea name="problem" rows="5" className="form-data"></textarea>
-          <label>Input Format</label>
-          <textarea name="inputFormat" rows="2" className="form-data"></textarea>
-          <label>Output Format</label>
-          <textarea name="outputFormat" rows="2" className="form-data"></textarea>
-          <label>Note</label>
-          <textarea name="note" rows="2" className="form-data"></textarea>
-          <label className="mb10">Sample Testcase</label>
-          <div className="testCase">
-            <div>
-              <label>Input</label>
-              <textarea name="sampleTestcaseInput" rows="5" className="form-data"></textarea>
-            </div>
-            <div>
-              <label>Output</label>
-              <textarea name="sampleTestcaseOutput" rows="5" className="form-data"></textarea>
-            </div>
-          </div>
-          <label className="mb10">Main Testcase</label>
-          <div className="testCase">
-            <div>
-              <label>Input</label>
-              <textarea name="mainTestcaseInput" rows="5" className="form-data"></textarea>
-            </div>
-            <div>
-              <label>Output</label>
-              <textarea name="mainTestcaseOutput" rows="5" className="form-data"></textarea>
-            </div>
-          </div>
-          <label>Explanation</label>
-          <textarea name="explanation" rows="2" className="form-data"></textarea>
-          <label>Max Score</label>
-          <input type="text" name="maxScore" className="form-data" />
-          <label>Difficulty</label>
-          <select name="difficulty" className="form-data">
-            <option value="easy">Easy</option>
-            <option value="Normal">Normal</option>
-            <option value="Hard">Hard</option>
-          </select>
-
-          <input type="submit" value="Submit" name="submit" className="button" />
-        </form> */}
       </div>
     );
   }
