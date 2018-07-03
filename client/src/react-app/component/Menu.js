3fadb569372bdcb39cf6376b2beeddb51d7c6dbd
Abhishek Kumar <abhishek.kumar@mountblue.io>
2018-07-03 20:49:01 +0530

    start to implement login

--- client/src/react-app/component/Menu.js
+++ client/src/react-app/component/Menu.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './Menu.css';
-import {Link} from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import Home from '../container/Home';
 // import AccountCircle from '@material-ui/icons/AccountCircle';
 
 class Menu extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+      isLogin: false
+    };
+  }
+
+  logOut() {
+    this.setState({isLogin: false});
+    console.log('called');
+    localStorage.clear();
+  }
+
   render() {
+    let logOutButton = null;
+    if (localStorage.getItem('ptok')) {
+      logOutButton = <button onClick={() => this.logOut()}>Logout</button>;
+    }
+
+    if(this.state.isLogin) {
+      console.log('from islogin');
+      <Home />;
+    }
+
     return (
       <div className='Menu'>
         <span>
@@ -16,6 +41,9 @@ class Menu extends React.Component {
         </span>
         <span>
           <Link to="/profile" className="Link">{localStorage.getItem('name')}</Link>
+        </span>
+        <span>
+          {logOutButton}
         </span>
       </div>
     );
