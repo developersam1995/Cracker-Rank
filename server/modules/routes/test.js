b3ad1ca11f8fc953256765d815f1051a29fbf7b7
jubinayoob <jubin.ayoob@mountblue.io>
2018-07-03 18:49:16 +0530

    Adds api for test-profile

--- server/modules/routes/test.js
+++ server/modules/routes/test.js
@@ -7,7 +7,7 @@ const TestsController = require('../controllers/test');
 
 router.route('/').post(passportJWT, TestsController.insert);
 router.route('/').get(passportJWT, TestsController.get);
-
+router.route('/profile').get(passportJWT,TestsController.getTestProfile);
 router.route('/register/:testId').post(passportJWT, TestsController.register);
     
 module.exports = router;
